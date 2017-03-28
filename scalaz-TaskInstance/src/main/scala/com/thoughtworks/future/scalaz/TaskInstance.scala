e037912078a99955ca1df23d5844fb7c5ba088f6
Yang Bo <atryyang@thoughtworks.com>
2017-03-28 14:43:18 +0800

    Add final modifier

--- scalaz-TaskInstance/src/main/scala/com/thoughtworks/future/scalaz/TaskInstance.scala
+++ scalaz-TaskInstance/src/main/scala/com/thoughtworks/future/scalaz/TaskInstance.scala
@@ -11,41 +11,44 @@ import scalaz.MonadError
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
 class TaskInstance extends MonadError[Task, Throwable] {
-  override def raiseError[A](e: Throwable) = {
+  override final def raiseError[A](e: Throwable) = {
     new Continuation.Return[A, Unit](Failure(e)) with Task[A]
   }
 
-  override def handleError[A](fa: Task[A])(f: (Throwable) => Task[A]) = {
+  override final def handleError[A](fa: Task[A])(f: (Throwable) => Task[A]) = {
     new Continuation.HandleError(fa, f) with Task[A]
   }
 
-  final def map[A, B](fa: Continuation[A, Unit])(f: A => B) = {
+  private def map[A, B](fa: Continuation[A, Unit])(f: A => B) = {
     new Continuation.Map(fa, f) with Task[B]
   }
-  override def map[A, B](fa: Task[A])(f: A => B) = {
+  override final def map[A, B](fa: Task[A])(f: A => B) = {
     this.map(fa: Continuation[A, Unit])(f)
   }
 
-  override def ap[A, B](fa: => Task[A])(f: => Task[A => B]): Task[B] = {
+  override final def ap[A, B](fa: => Task[A])(f: => Task[A => B]): Task[B] = {
     map(Zip(fa, f)) { pair: (A, A => B) =>
       pair._2(pair._1)
     }
   }
 
-  override def bind[A, B](fa: Task[A])(f: (A) => Task[B]) = {
+  override final def bind[A, B](fa: Task[A])(f: (A) => Task[B]) = {
     new Continuation.FlatMap(fa, f) with Task[B]
   }
 
-  override def point[A](a: => A) = {
+  override final def point[A](a: => A) = {
     new Continuation.Return[A, Unit](Success(a)) with Task[A]
   }
 
-  override def apply2[A, B, C](fa: => Task[A], fb: => Task[B])(f: (A, B) => C): Task[C] = {
+  override final def apply2[A, B, C](fa: => Task[A], fb: => Task[B])(f: (A, B) => C): Task[C] = {
     map(Zip(fa, fb)) { pair =>
       f(pair._1, pair._2)
     }
   }
 }
 
-// val f: () => Future[Int] = ???
-// val task: Task[Int]
+object TaskInstance {
+
+  implicit def apply: TaskInstance = new TaskInstance {}
+
+}
