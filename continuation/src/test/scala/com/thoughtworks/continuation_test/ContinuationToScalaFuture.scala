df507c1b581ae69b9d9347ce4f1791d2939bec8e
Yang Bo <pop.atry@gmail.com>
2017-07-25 11:43:21 +0800

    Simplify ContinuationToScalaFuture

--- continuation/src/test/scala/com/thoughtworks/continuation_test/ContinuationToScalaFuture.scala
+++ continuation/src/test/scala/com/thoughtworks/continuation_test/ContinuationToScalaFuture.scala
@@ -1,24 +1,14 @@
 package com.thoughtworks.continuation_test
 
 import scala.language.implicitConversions
-import scala.concurrent.Promise
 import com.thoughtworks.continuation._
-import org.scalactic.source
-import org.scalatest.{Assertion, AsyncFreeSpec}
 
 /**
   * @author 杨博 (Yang Bo)
   */
-trait ContinuationToScalaFuture extends AsyncFreeSpec {
+trait ContinuationToScalaFuture {
 
-  implicit def continuationToScalaFuture[A](continuation: Continuation[Unit, A]): scala.concurrent.Future[A] = {
-    // Avoid recursive call to continuationToScalaFuture
-    val continuationToScalaFuture = DummyImplicit.dummyImplicit
-
-    import com.thoughtworks.continuation._
-
-    val promise = Promise[A]
-    continuation.onComplete(promise.success)
-    promise.future
+  implicit def continuationToScalaFuture[A](continuation: UnitContinuation[A]): scala.concurrent.Future[A] = {
+    continuation.toScalaFuture
   }
 }
